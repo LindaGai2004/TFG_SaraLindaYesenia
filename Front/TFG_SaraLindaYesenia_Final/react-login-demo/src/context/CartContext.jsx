713b600dd5bee9e0525ff4ee25076a7e231c4e8c
yingaruca <xct1um@gmail.com>
2026-01-09 10:57:29 +0100

    Add explanatory comments to CartContext

--- Front/TFG_SaraLindaYesenia_Final/react-login-demo/src/context/CartContext.jsx
+++ Front/TFG_SaraLindaYesenia_Final/react-login-demo/src/context/CartContext.jsx
@@ -1,34 +1,41 @@
+//El CartContext sirve para guardar el estado del carrito globalmente
+//Persiste en localStorage
+
 import { createContext, useContext, useEffect, useState } from 'react';
 
 const CartContext = createContext();
+
 export const useCart = () => useContext(CartContext);
 
 export function CartProvider({ children }) {
+  //Un carrito es una lista de items que se inicializa vacio
   const [cartItems, setCartItems] = useState([]);
 
-  // Load cart from localStorage on app start
+  //Abrir el carrito guardado
   useEffect(() => {
     const storedCart = localStorage.getItem('cartItems');
     if (storedCart) {
       try {
         setCartItems(JSON.parse(storedCart));
       } catch (e) {
-        console.error('Error parsing cartItems from localStorage', e);
+        console.error('No se pudieron cargar los productos del carrito', e);
         setCartItems([]);
       }
     }
   }, []);
 
-  // Save cart to localStorage when it changes
+  // Se usa cuando cambia el estado del carrito y lo mantiene(persiste)
   useEffect(() => {
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
   }, [cartItems]);
 
+  //Visible para los componentes
   const value = {
     cartItems,
     setCartItems, // we’ll replace this later with addToCart, removeFromCart
   };
 
+  //Significa que los componentes de este Provider pueden acceder al carrito (route, pages, botones pueden leerlo)
   return (
     <CartContext.Provider value={value}>
       {children}
