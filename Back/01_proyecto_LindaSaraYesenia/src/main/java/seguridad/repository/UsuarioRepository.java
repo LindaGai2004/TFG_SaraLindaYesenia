fedf0736bcb2657dc0b971344cec0391c37c161b
lindaaaaaaaaaaaaa <gaizhuoya@gmail.com>
2026-01-18 21:53:42 +0100

    "CAMBIO 18/01(3)"

--- Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/repository/UsuarioRepository.java
+++ Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/repository/UsuarioRepository.java
@@ -19,6 +19,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 	List<Usuario> findByPerfil_IdPerfilAndEmailContainingIgnoreCase (int idPerfil, String email);
 	List<Usuario> findByPerfil_IdPerfilAndNombreContainingIgnoreCase (int idPerfil, String nombre);
 	List<Usuario> findByPerfil_IdPerfilAndUsernameContainingIgnoreCase (int idPerfil, String username);
-
+	List<Usuario> findByPerfil_IdPerfilAndApellidosContainingIgnoreCase(int idPerfil, String apellidos);
 	
 }
