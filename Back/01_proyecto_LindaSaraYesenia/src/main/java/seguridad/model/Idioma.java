98915aff435e0e4627014275217d2a7f3b0ac4df
lindaaaaaaaaaaaaa <gaizhuoya@gmail.com>
2026-01-18 14:09:06 +0100

    "cambio 18/01(02)"

--- Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/model/Idioma.java
+++ Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/model/Idioma.java
@@ -2,12 +2,13 @@ package seguridad.model;
 
 import java.io.Serializable;
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
