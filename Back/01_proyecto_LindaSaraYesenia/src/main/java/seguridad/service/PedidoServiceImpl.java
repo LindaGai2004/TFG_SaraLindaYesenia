564e6675ecbe4c96fc817721532fca617bb1acea
yingaruca <xct1um@gmail.com>
2026-01-22 12:53:47 +0100

    Cambio de 22-01 Sprigtools at 12:54pm

--- Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/service/PedidoServiceImpl.java
+++ Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/service/PedidoServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import seguridad.model.DetallePedido;
 import seguridad.model.EstadoPedido;
 import seguridad.model.Pedido;
 import seguridad.model.Producto;
@@ -13,14 +14,17 @@ import seguridad.model.Usuario;
 import seguridad.model.Dto.PedidoItemRequest;
 import seguridad.model.Dto.PedidoRequest;
 import seguridad.repository.PedidoRepository;
+import seguridad.repository.ProductoRepository;
 import seguridad.repository.UsuarioRepository;
+import seguridad.repository.DetallePedidoRepository;
 @Service
 public class PedidoServiceImpl implements PedidoService {
 
 	@Autowired
 	private PedidoRepository prepo;
+	private ProductoRepository porepo;
 	private UsuarioRepository urepo;
-	
+	private  DetallePedidoRepository dprepo;
 	@Override
 	public Pedido insertPedido(PedidoRequest request) {
 		Usuario usuario = urepo.findById(request.getIdUsuario())
@@ -34,9 +38,22 @@ public class PedidoServiceImpl implements PedidoService {
 		pedido = prepo.save(pedido);
 		
 		for (PedidoItemRequest item:request.getItems()) {
-			Producto producto = prepo.findById(item.getIdProducto());
+			Producto producto = porepo.findById(item.getIdProducto())
+				.orElseThrow(()-> new RuntimeException("No existe el producto"));
+			double precio = producto.getPrecio();
+			int cantidad = item.getCantidad();
+			DetallePedido detalle = new DetallePedido();
+			detalle.setPedido(pedido);
+			detalle.setProducto(producto);
+			detalle.setCantidad(cantidad);
+			detalle.setPrecioUnidad(precio);
+			
+			dprepo.save(detalle);
+			total += precio * cantidad;
+			
 			
 		}
+		pedido.setTotal(total);
 		return prepo.save(pedido);
 	}
 	@Override
