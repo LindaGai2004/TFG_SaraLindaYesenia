05d1f2d4fd897e9ecdb55d2435e242093e6cf703
Yesenia <xct1um@gmail.com>
2026-01-30 00:39:25 +0100

    Avance 30-01 noche

--- Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/service/CarritoServiceImpl.java
+++ Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/service/CarritoServiceImpl.java
@@ -40,10 +40,12 @@ public class CarritoServiceImpl implements CarritoService {
 
 		Pedido carritoExiste = prepo.findByUsuario_IdUsuarioAndEstado(idUsuario, EstadoPedido.CARRITO)
 		.orElse(null);
-		
+		//Verificar si ya existe Pedido con estado 'CARRITO' para un usuario
 		if (carritoExiste!= null) {
+			//Devuelve carrito
 			return carritoExiste;
 		}else {
+			//Crea uno y lo devuelve
 			Pedido carritoNuevo = new Pedido();
 			carritoNuevo.setUsuario(usuario);
 			carritoNuevo.setEstado(EstadoPedido.CARRITO);
@@ -53,45 +55,75 @@ public class CarritoServiceImpl implements CarritoService {
 		}
 		
 	}
-
+	//Funcionalidad "Add to cart" SOLO ADD (+)
 	@Override
-	public Pedido añadirItem(Integer idUsuario, CarritoItemRequest item) {
+	public Pedido addItem(Integer idUsuario, CarritoItemRequest item) {
+		//Obtener carrito si existe sino lo crea
 		Pedido carrito = createCarrito(idUsuario);
+		//Verificar que exista el producto
 		Producto producto = porepo.findById(item.getIdProducto())
 				.orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+		//Verificar disponibilidad
+		if(producto.getStock() <= 0) {
+			throw new RuntimeException("¡Producto sin stock!");
+		}
+		//Si el producto existe en el carrito aumentar cantidad sino crear nueva linea detallePedido
 		DetallePedido itemExiste = dprepo.findByPedido_IdPedidoAndProducto_IdProducto(
 			carrito.getIdPedido(), producto.getIdProducto()).orElse(null);
+		
+		//La cantidad del producto en el carrito actual
+		int qtyEnCarrito = 0;
 		if (itemExiste != null) {
-			int cambiarQty = itemExiste.getCantidad() + item.getCantidad();
-			itemExiste.setCantidad(cambiarQty);
-			dprepo.save(itemExiste);
-			return carrito;
-		}else {
-			DetallePedido itemNuevo = new DetallePedido();
-			itemNuevo.setPedido(carrito);
-			itemNuevo.setProducto(producto);
-			itemNuevo.setCantidad(item.getCantidad());
-			itemNuevo.setPrecioUnidad(producto.getPrecio());
-			return carrito;
+			qtyEnCarrito = itemExiste.getCantidad();
 		}
 		
+		//Asegurar q no exceda la cantidad de stock disponible
+		int qtyFinal = qtyEnCarrito + item.getCantidad();
+
+	    if (qtyFinal > producto.getStock()) {
+	        throw new RuntimeException("¡Producto sin stock!");
+	    }
+
+	    if (itemExiste != null) {
+	        itemExiste.setCantidad(qtyFinal);
+	        dprepo.save(itemExiste);
+	    } else {
+	        DetallePedido itemNuevo = new DetallePedido();
+	        itemNuevo.setPedido(carrito);
+	        itemNuevo.setProducto(producto);
+	        itemNuevo.setCantidad(item.getCantidad());
+	        itemNuevo.setPrecioUnidad(producto.getPrecio());
+	        dprepo.save(itemNuevo);
+	    }
+
+	    return carrito;
+		
 	}
-	//cambiar la cantidad de un detallepedido existente
+	//Cambiar la cantidad de un detallePedido existente (+/-) o eliminar si < 0
 	@Override
 	public Pedido updateItem(Integer idUsuario, CarritoItemRequest item) {
+		//Obtener carrito
 	    Pedido carrito = createCarrito(idUsuario);
+	    //Obtener producto
+	    Producto producto = porepo.findById(item.getIdProducto())
+	    		.orElseThrow(() -> new RuntimeException("Producto no existe"));
+	    //Encontrar detallePedido
 	    DetallePedido detalle = dprepo.findByPedido_IdPedidoAndProducto_IdProducto(
-	                    carrito.getIdPedido(),item.getIdProducto())
+	                    carrito.getIdPedido(), producto.getIdProducto())
 	            .orElseThrow(() -> new RuntimeException("El producto no está en el carrito"));
 
 	    if (item.getCantidad() <= 0) {
 	        dprepo.delete(detalle);
 	        return carrito;
-	    }else {
-		    detalle.setCantidad(item.getCantidad());
-		    dprepo.save(detalle);
-		    return carrito;
 	    }
+	    if (item.getCantidad() > producto.getStock()) {
+	        throw new RuntimeException("¡Producto sin stock!");
+	    }
+	    //Para hacer update de la cantidad
+	    detalle.setCantidad(item.getCantidad());
+	    dprepo.save(detalle);
+	    return carrito;
+	    
 	}
 
 	@Override
@@ -106,23 +138,27 @@ public class CarritoServiceImpl implements CarritoService {
 
 	@Override
 	public PedidoResponse confirmarCarrito(Integer idUsuario) {
+		//Get carrito activo (Pedido en estado 'CARRITO')
 		Pedido carrito = prepo.findByUsuario_IdUsuarioAndEstado(idUsuario, EstadoPedido.CARRITO)
 			.orElseThrow(() -> new RuntimeException ("No hay carrito activo"));
+		//Get detalles
 		List<DetallePedido> detalles = dprepo.findByPedido_IdPedido(carrito.getIdPedido());
 		if (detalles.isEmpty()) {
 	        throw new RuntimeException("El carrito está vacío");
 	    }
+		//Calcular total
 		double total = 0.0;
 		for (DetallePedido d: detalles) {
-			
 			Producto producto = d.getProducto();
 			int cantidad = d.getCantidad();
+			//Verificar Stock
 			if(producto.getStock() < cantidad) {
 				 throw new RuntimeException(
 		                    "No hay stock para " + producto.getNombreProducto());
 			}
-			
+			//Actualizar stock
 			producto.setStock(producto.getStock() - cantidad);
+			//Actualizar estado producto if stock=0
 			if(producto.getStock() == 0) {
 				producto.setEstadoProducto(EstadoProducto.AGOTADO);
 			}
@@ -130,7 +166,7 @@ public class CarritoServiceImpl implements CarritoService {
 			total += d.getPrecioUnidad() * cantidad;
 			
 		}
-		
+		//Pedido realizado con total
 		carrito.setEstado(EstadoPedido.REALIZADO);
 		carrito.setFechaVenta(LocalDate.now());
 		carrito.setTotal(total);
