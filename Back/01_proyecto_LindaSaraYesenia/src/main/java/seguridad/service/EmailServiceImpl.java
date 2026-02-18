1e8c140cf006b0f7ac833861f7a9f29abfc78017
yingaruca <xct1um@gmail.com>
2026-02-18 13:25:40 +0100

    18-02 Clase

--- Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/service/EmailServiceImpl.java
+++ Back/01_proyecto_LindaSaraYesenia/src/main/java/seguridad/service/EmailServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import org.springframework.core.io.ByteArrayResource;
 
 import jakarta.mail.internet.MimeMessage;
@@ -13,16 +15,23 @@ public class EmailServiceImpl implements EmailService{
 
 	@Autowired
 	private JavaMailSender mailSender;
-	
+	@Autowired
+	private TemplateEngine templateEngine;
 	@Override
 	public void enviarFacturaAdjunto(String to, Factura factura, byte[] pdf) 
 			throws Exception{
 		MimeMessage mensaje = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(mensaje, true);
+		MimeMessageHelper helper = new MimeMessageHelper(mensaje, true, "UTF-8");
+		//Contexto de Thymeleaf
+		Context context = new Context();
+		context.setVariable("factura", factura);
+		context.setVariable("usuario",factura.getPedido().getUsuario());
+		//Plantilla HTML
+		String contenidoHtml = templateEngine.process("emailFactura", context);
+		
 		helper.setTo(to);
 		helper.setSubject("Tu factura " + factura.getNumFactura());
-		helper.setText("Gracias por tu compra. Adjuntamos tu factura");
-		
+		helper.setText(contenidoHtml, true);
 		helper.addAttachment(factura.getNumFactura() + ".pdf", new ByteArrayResource(pdf));
 		mailSender.send(mensaje);
 	}
