99ccb05bd5a591f2fecee9eccc66e2a777b32531
Sara <sarayingshavilas@gmail.com>
2026-02-23 23:34:17 +0100

    todo correcto

--- Front/TFG_SaraLindaYesenia_Final/react-login-demo/src/components/Sidebar_dashboard.jsx
+++ Front/TFG_SaraLindaYesenia_Final/react-login-demo/src/components/Sidebar_dashboard.jsx
@@ -81,7 +81,7 @@ export default function Sidebar({ currentPage, onNavigate, onLogout }) {
         ))}
       </nav>
 
-                {/* Logout */}
+      {/* Logout */}
           <a className="app-sidebar-link logout-link" onClick={logout}>
             <span>Cerrar sesión</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2">
